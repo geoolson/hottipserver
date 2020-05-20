5ccc9d171eb54d013051953ac56f2d11b30e670b
Geoffrey <geoolson@pdx.edu>
2020-05-20 02:11:13 -0700

    I've added new API routes and SQL queries to update the database with new tips and to fetch tips from the database

--- createDB.js
+++ createDB.js
@@ -3,20 +3,33 @@ var db = new sqlite3.Database('tips.db');
  
 db.serialize(function() {
   db.run(`CREATE TABLE tips (
-      user_id INT,
+      username TEXT,
       lat REAL,
       lng REAL,
       tip INT,
       sub_total INT
       )`);
+
+    db.run(`CREATE TABLE users (
+      username TEXT,
+      password TEXT,
+      permission INT
+      )`
+    )
  
   var stmt = db.prepare("INSERT INTO tips VALUES (?,?,?,?,?)");
   for (var i = 0; i < 10; i++) {
       stmt.run(0, 45,45,i,i);
   }
   stmt.finalize();
+  var stmt = db.prepare("INSERT INTO users VALUES (?,?,?)");
+  stmt.run("test", "test", 100);
+  stmt.finalize();
  
   db.each("SELECT * FROM tips", function(err, row) {
+      console.log(row);
+  });
+  db.each("SELECT * FROM users", function(err, row) {
       console.log(row);
   });
 });
--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = 8080;
+const session = require('express-session')
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('tips.db');
+
+app.use(express.urlencoded({extended: true}));
+app.use(session({secret: "session", resave: false, saveUninitialized: true}));
 
 const lngRange = 0.4;
 const latRange = 0.2;
@@ -11,10 +17,54 @@ app.use(cors());
 
 app.use(express.urlencoded({extended: true}));
 
+app.get('/login/*/*', (req, res) => {
+    const pathSplit = req.path.split('/');
+    const username = pathSplit[2];
+    const password = pathSplit[3];
+    db.serialize(function () {
+        let stmt = db.prepare(`
+        SELECT *
+        FROM users
+        WHERE username=?
+        AND password=?`);
+        console.log(stmt.get(username, password));
+    });
+});
+
 app.get('/api/*/*', (req, res) => {
+    const pathSplit = req.path.split('/');
+    const lat = parseFloat(pathSplit[2]);
+    const lng = parseFloat(pathSplit[3]);
+    let result = []
+    db.serialize(function () {
+        db.each(`
+        SELECT *
+        FROM tips
+        WHERE
+        lat > ${lat} - ${latRange}
+        AND lat <  ${lat} + ${latRange}
+        AND lng > ${lng} - ${lngRange}
+        AND lng < ${lng} + ${lngRange}
+        `, function(err, row){
+            result = [...result, row]
+        });
+        res.json(result);
+    });
 });
 
 app.post('/api/addtip/*/*/*/*', (req, res) => {
+    const pathSplit = req.path.split('/');
+    const lat = parseFloat(pathSplit[3]);
+    const lng = parseFloat(pathSplit[4]);
+    const tip = parseFloat(pathSplit[5]);
+    const subtotal = parseFloat(pathSplit[6]);
+    console.log(lat, lng, tip, subtotal)
+    db.serialize(function () {
+        let stmt = db.prepare("INSERT INTO tips VALUES (?,?,?,?,?)");
+        stmt.run("testing", lat, lng, tip, subtotal);
+        stmt.finalize();
+        res.end();
+    });
 });
 
 app.listen(port, () => {
